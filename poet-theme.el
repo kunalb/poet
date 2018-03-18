260626b84a02c7b06997fb8d8bb7eed61da7eff4
Kunal Bhalla <bhalla.kunal@gmail.com>
2018-03-18 16:51:34 -0400

    Add some inline documentation

--- poet-theme.el
+++ poet-theme.el
@@ -14,6 +14,31 @@
 ;; org-mode and markdown-mode.  Code blocks, tables, etc are
 ;; formatted in monospace text with the appropriate backgrounds.
 
+;; Recommended customizations for using this theme
+;;
+;; - Set up the base fonts you'd like to use in Emacs before loading Poet
+;;     (set-face-attribute 'default nil :height 130)
+;;     (set-face-attribute 'fixed-pitch nil :family "Fira Code")
+;;     (set-face-attribute 'variable-pitch nil :family "Georgia")
+;;   On loading this theme captures the default and treats that for fixed-pitch
+;;   rendering.
+;;
+;; - Enable variable pitch mode for editing text
+;; (add-hook 'text-mode-hook
+;;            (lambda ()
+;;             (variable-pitch-mode 1))
+;;
+;; - Some other modes I like to enable/disable
+;;     (olivetti-mode 1)        ;; Centers text in the buffer
+;;     (flyspell-mode 1))       ;; Catch Spelling mistakes
+;;     (typo-mode 1))           ;; Good for symbols like em-dash
+;;     (blink-cursor-mode 0)    ;; Reduce visual noise
+;;     (linum-mode 0)           ;; No line numbers for prose
+;;
+;; - And prettier org mode bullets:
+;;     (setq org-bullets-bullet-list
+;;         '("◉" "○"))
+;;     (org-bullets 1)
 
 ;;; Code:
 
